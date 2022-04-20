8e011a8d97be8e4bbfc27064dc209c28fc4c99c4
Morales97 <danimoralesbrotons@gmail.com>
2022-04-20 19:14:25 +0200

    Update evaluateSSL.py

--- evaluateSSL.py
+++ evaluateSSL.py
@@ -110,10 +110,9 @@ def evaluate(model, dataset, deeplabv2=True, ignore_label=250, save_dir=None, pr
             '''
             model.cuda()
             output = model(normalize(image, dataset).cuda())
-            # output = F.interpolate(output, size=(label.shape[1], label.shape[2]), mode="bilinear", align_corners=True)
+            output = F.interpolate(output, size=(label.shape[1], label.shape[2]), mode="bilinear", align_corners=True)
 
             criterion = CrossEntropy2d(ignore_label=ignore_label).cuda()
-            pdb.set_trace()
             loss = criterion(output, label)
 
             total_loss.append(loss.item())
