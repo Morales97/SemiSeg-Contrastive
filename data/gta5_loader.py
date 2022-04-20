b3ff14a92109af17726f32a1364e85f6410ee175
Morales97 <danimoralesbrotons@gmail.com>
2022-04-20 11:46:21 +0200

    .

--- data/gta5_loader.py
+++ data/gta5_loader.py
@@ -137,7 +137,7 @@ class gtaLoader(data.Dataset):
 
             lbl = imageio.imread(lbl_path)
             lbl = np.array(lbl, dtype=np.uint8)
-
+            print('gucci')
             if self.augmentations is not None:
                 img, lbl = self.augmentations(img, lbl)
             if self.is_transform:
